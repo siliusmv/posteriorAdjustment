9c28c476d48823504d12d1cf17419ee999998226
Silius M. Vandeskog <siliusv@gmail.com>
2022-08-15 14:37:17 +0200

    Document posterior-adjustment functions

--- R/posterior-adjustment.R
+++ R/posterior-adjustment.R
@@ -1,5 +1,6 @@
 
-
+#' Compute the matrix C that solves the equation
+#' C H^-1 C^T = H^-1 J H^-1
 #' @export
 get_C = function(H, J) {
   if (length(H) == 1 && length(J) == 1) return(get_C_univ(H, J))
@@ -11,6 +12,7 @@ get_C = function(H, J) {
   C
 }
 
+# Compute C when H and J are scalars
 get_C_univ = function(H, J) {
   M_1 = sqrt(1 / H)
   M_2 = sqrt(J / H^2)

81b57ee25cb1d501cb5cbb07a2178aa80e393383
Silius M. Vandeskog <siliusv@gmail.com>
2022-08-25 14:23:51 +0200

    Update the Rcpp test script

--- exec/tests/1-rcpp.R
+++ exec/tests/1-rcpp.R
@@ -2,44 +2,36 @@ devtools::load_all()
 library(microbenchmark)
 library(mvtnorm)
 library(mvnfast)
-library(bayesm)
-
-set.seed(123)
-sigma = bayesm::rwishart(10, diag(8))$IW
-means = rnorm(8)
-X = mvtnorm::rmvnorm(900000, means, sigma)
-X_t = t(X)
-nugget = 2
-
-vals = list(
-  dmvnorm = mvtnorm::dmvnorm(X, means, sigma, log = TRUE),
-  dmvnorm_arma = dmvnorm_arma(X_t, means, sigma, TRUE),
-  dmvn = mvnfast::dmvn(X, means, sigma, log = TRUE))
-sapply(vals, function(x) summary(as.numeric(x - vals[[1]])))
-
-m1 = microbenchmark::microbenchmark(
-  dmvnorm = mvtnorm::dmvnorm(X, means, sigma, log = TRUE),
-  dmvnorm_arma = dmvnorm_arma(X_t, means, sigma, TRUE),
-  dmvn = mvnfast::dmvn(X, means, sigma, log = TRUE))
-m1
-
-# ==============================================================================
-# Test dconditional
-# ==============================================================================
-
-library(parallel)
-library(pbapply)
-library(INLA)
-library(Matrix)
 library(sf)
+library(ggplot2)
+library(dplyr)
+library(tidyr)
+library(INLA)
+library(inlabru)
 
+# In this script we evaluate the speed and correctness of the Rcpp functions used
+# for computing log-likelihoods for the spatial conditional extremes model.
+# The evaluation is performed using data from the case study, where we
+# compute log-likelihoods both using the Rcpp functions, and using functions written
+# in plain R. The values and computation speeds are then compared for the R and Rcpp functions.
+
+# ==============================================================================
+# Decide necessary model parameters:
+# ==============================================================================
 threshold = qlaplace(.95) # The threshold t for defining the conditional extremes model
-n_cores = 6 # Run code in parallel
+n_cores = 15 # Run code in parallel. Check yourself that this is an ok number
 r = 5 # Radius used for computing aggregated empirical distribution functions
+
+# ==============================================================================
+# Load the radar data
+# ==============================================================================
 radar = readRDS(file.path(downloads_dir(), "radar.rds"))
 coords = st_coordinates(radar$coords)
 n_loc = nrow(coords)
 
+# ==============================================================================
+# Transform the data to Laplace margins
+# ==============================================================================
 cl = parallel::makeForkCluster(n_cores)
 transformed_data = pbapply::pblapply(
   X = 1:n_loc,
@@ -56,6 +48,11 @@ transformed_data = pbapply::pblapply(
 parallel::stopCluster(cl)
 transformed_data = do.call(cbind, transformed_data)
 
+# ==============================================================================
+# Choose all conditioning sites for the global model
+# ==============================================================================
+
+# Create a grid of conditioning sites with resolution (delta_s0 x delta_s0)
 delta_s0 = 4
 x_coords = coords[, 1] |> unique() |> sort()
 y_coords = coords[, 2] |> unique() |> sort()
@@ -64,6 +61,7 @@ s0_locs = expand.grid(
   y = y_coords[seq(delta_s0, length(y_coords), by = delta_s0)]) |>
   as.matrix()
 
+# Find out which indices the chosen conditioning sites correspond to
 s0_index = lapply(
   X = 1:nrow(s0_locs),
   FUN = function(i) which(coords[, 1] == s0_locs[i, 1] & coords[, 2] == s0_locs[i, 2]))
@@ -71,6 +69,10 @@ s0_index = s0_index[sapply(s0_index, length) > 0] |>
   unlist() |>
   unname()
 
+# ==============================================================================
+# Extract observations in a nonregular grid around conditioning sites that
+# exceed the threshold
+# ==============================================================================
 thinning = c(1, 2, 4, 6, 8, 16, 32)
 data = extract_extreme_fields(
   data = transformed_data,
@@ -80,131 +82,139 @@ data = extract_extreme_fields(
   n = thinning,
   r = cumsum(4 * thinning))
 
+# ==============================================================================
+# Create the mesh and spde
+# ==============================================================================
 mesh = inla.mesh.2d(
   loc = coords,
   boundary = list(inla.nonconvex.hull(coords, convex = -.1),
                   inla.nonconvex.hull(coords, convex = -1)),
   max.edge = c(5, 30))
+mesh$n
+# plot(mesh)
+# points(coords)
+
 spde = inla.spde2.pcmatern(mesh, prior.range = c(60, .95), prior.sigma = c(5, .05))
 
+# Compute the distances to the conditioning sites from the mesh nodes,
+# and add the information to `data`
 data$dist_to_s0_from_mesh = list()
 for (i in seq_along(data$s0)) {
   data$dist_to_s0_from_mesh[[i]] = dist_euclid(mesh$loc[, 1:2], data$s0[[i]])
 }
 
-get_a_func = function(theta) {
+# ==============================================================================
+# Compute log-likelihoods and compare computation times
+# ==============================================================================
+
+# Define the composite log-likelihood for the global conditional
+# extremes model. The function below is a wrapper for the function loglik_conditional
+#
+# Input variables:
+# theta: A vector of parameters of length 5 or 6, depending on the value of rho_b.
+# y, y0, dist_to_s0, dist_to_s0_from_mesh, A, n_cores: See ?loglik_conditional for more info
+# rho_b: Either NULL or a double. If is.null(rho_b), then theta has length 6, and
+#   we try to estimate rho_b. Else, theta has length 5, and rho_b is fixed and equal
+#   to the given value.
+# sum_terms: A boolean describing wheter we should compute the sum of the log-likelihood,
+#   or if we should remove one value for each threshold exceedance.
+# no_beta: A boolean. Should we compute the composite log-likelihood using the
+#   dconditional(_r)_no_beta() versions or using the dconditional(_r)() versions?
+loglik = function(theta,
+                  y,
+                  y0,
+                  dist_to_s0,
+                  dist_to_s0_from_mesh,
+                  A,
+                  use_r,
+                  rho_b = NULL,
+                  sum_terms = TRUE,
+                  no_beta = TRUE,
+                  n_cores = 1) {
+  if (is.null(rho_b)) {
+    stopifnot(length(theta) == 6)
+    rho_b = exp(theta[3])
+    log_rho = theta[4]
+    log_sigma = theta[5]
+    tau = exp(theta[6])
+  } else {
+    stopifnot(length(theta) == 5)
+    log_rho = theta[3]
+    log_sigma = theta[4]
+    tau = exp(theta[5])
+  }
   lambda = exp(theta[1])
   kappa = exp(theta[2])
-  function(y, dist) {
+  Q = INLA::inla.spde2.precision(spde, c(log_rho, log_sigma))
+  cov_mat = as.matrix(Matrix::solve(Q))
+  a_func = function(y, dist) {
     alpha = exp(- (dist / lambda)^kappa)
     matrix(rep(y, each = length(alpha)) * rep(alpha, length(y)),
-           nrow = length(dist),
-           ncol = length(y))
+           nrow = length(dist), ncol = length(y))
   }
-}
-get_b_func = function(theta) {
-  rho = exp(theta)
-  function(y, dist) {
-    tmp = dist / rho
+  b_func = function(y, dist) {
+    tmp = dist / rho_b
     tmp[tmp < 1e-9] = 1e-9
     b = sqrt(1 - exp(-2 * tmp))
-    matrix(rep(b, length(y)), nrow = length(dist), ncol = length(y))
+    res = matrix(rep(b, length(y)), nrow = length(dist), ncol = length(y))
+    if (!no_beta) {
+      # Add some small random noise so we won't use the dconditional(_r)_no_beta() functions
+      set.seed(1)
+      res = res + rnorm(length(res), sd = 1e-8)
+    }
+    res
+  }
+  res = loglik_conditional(
+    y = y,
+    y0 = y0,
+    a_func = a_func,
+    b_func = b_func,
+    sigma = cov_mat,
+    tau = tau,
+    dist_to_s0 = dist_to_s0,
+    dist_to_s0_from_mesh = dist_to_s0_from_mesh,
+    A = A,
+    n_cores = n_cores,
+    use_r = use_r)
+  if (sum_terms) res = sum(res)
+  res
+}
+
+# Prerequisites for computing the log-likelihoods
+theta = c(3.8, -.4, -.5, 2.3, .3, 4.2)
+A = lapply(data$obs_index, function(x) inla.spde.make.A(mesh, coords[x, ]))
+args = list(
+  theta = theta,
+  y = data$y,
+  y0 = data$y0,
+  dist_to_s0 = data$dist_to_s0,
+  dist_to_s0_from_mesh = data$dist_to_s0_from_mesh,
+  A = A,
+  sum_terms = FALSE)
+
+# Compute the log-likelihoods for all combinations of use_r and no_beta
+results = list()
+for (no_beta in c(TRUE, FALSE)) {
+  for (use_r in c(TRUE, FALSE)) {
+    args$use_r = use_r
+    args$no_beta = no_beta
+
+    time = proc.time()
+    ll = do.call(loglik, args)
+    time = proc.time() - time
+
+    name = paste0("use_r = ", use_r, ", no_beta = ", no_beta)
+    results[[name]] = list(ll = ll, time = time)
   }
 }
 
-theta = c(4, -.4, 1, 4, .5, 4)
-y = data$y
-y0 = data$y0
-dist_to_s0 = data$dist_to_s0
-dist_to_s0_from_mesh = data$dist_to_s0_from_mesh
-A_mats = lapply(data$obs_index, function(x) inla.spde.make.A(mesh, coords[x, ]))
-n_cores = n_cores
+# Compare log-scores. We see that the Rcpp functions and the R functions return the same values
+ll = sapply(results, `[[`, "ll")
+apply(ll[, 1:2], 1, function(x) max(x) - min(x)) |>
+  summary()
+apply(ll[, 3:4], 1, function(x) max(x) - min(x)) |>
+  summary()
 
-b_func = get_b_func(theta[3])
-cov_mat_func = get_Σ_func(theta[4:5], spde)$value
-tau = exp(theta[6])
-a_func = get_a_func(theta[1:2])
-
-lengths = c(length(y0), length(y), length(A_mats), length(dist_to_s0))
-stopifnot(all(lengths == lengths[1]))
-if (!is.null(dist_to_s0_from_mesh)) stopifnot(length(dist_to_s0_from_mesh) == length(y))
-
-i = 1
-b = b_func(1, dist_to_s0_from_mesh[[i]])
-x = y[[i]] - a_func(y0[[i]], dist_to_s0[[i]])
-A = A_mats[[i]]
-B = Matrix::Diagonal(length(b), as.numeric(b))
-B2 = do.call(cbind, rep(list(b), ncol(x)))
-sigma0 = cov_mat_func()
-nugget = 1 / tau
-
-args = list(
-  x = x,
-  A = A,
-  #B = B,
-  b = b,
-  sigma0 = sigma0,
-  nugget = nugget,
-  logd = TRUE,
-  na_rm = TRUE)
-args2 = args
-args2$b = NULL
-args2$B = B2
-
-d1 = local({
-  sigma = as.matrix(A %*% B %*% sigma0 %*% B %*% t(A)) + diag(nugget, nrow(A))
-  sapply(
-    X = 1:ncol(x),
-    FUN = function(i) {
-      good_index = which(!is.na(x[, i]))
-      mvtnorm::dmvnorm(x[good_index, i], sigma = sigma[good_index, good_index], log = TRUE)
-    })
-})
-d2 = local({
-  sigma = as.matrix(A %*% B %*% sigma0 %*% B %*% t(A)) + diag(nugget, nrow(A))
-  sapply(
-    X = 1:ncol(x),
-    FUN = function(i) {
-      good_index = which(!is.na(x[, i]))
-      mvnfast::dmvn(
-        x[good_index, i],
-        mu = rep(0, length(good_index)),
-        sigma = sigma[good_index, good_index],
-        log = TRUE)
-    })
-})
-d3 = do.call(dconditional_no_beta, args)
-d4 = do.call(dconditional, args2)
-summary(as.numeric(d1 - d2))
-summary(as.numeric(d1 - d3))
-summary(as.numeric(d1 - d4))
-
-m2 = microbenchmark::microbenchmark(
-  d1 = local({
-    sigma = as.matrix(A %*% B %*% sigma0 %*% B %*% t(A)) + diag(nugget, nrow(A))
-    sapply(
-      X = 1:ncol(x),
-      FUN = function(i) {
-        good_index = which(!is.na(x[, i]))
-        mvtnorm::dmvnorm(
-          x[good_index, i],
-          sigma = sigma[good_index, good_index],
-          log = TRUE)
-      })
-  }),
-  d2 = local({
-    sigma = as.matrix(A %*% B %*% sigma0 %*% B %*% t(A)) + diag(nugget, nrow(A))
-    sapply(
-      X = 1:ncol(x),
-      FUN = function(i) {
-        good_index = which(!is.na(x[, i]))
-        mvnfast::dmvn(
-          x[good_index, i],
-          mu = rep(0, length(good_index)),
-          sigma = sigma[good_index, good_index],
-          log = TRUE)
-      })
-  }),
-  d3 = do.call(dconditional_no_beta, args),
-  d4 = do.call(dconditional, args2))
-m2
+# Compare the computation times. The Rcpp functions are considerably faster than the R functions
+times = sapply(results, `[[`, "time")
+times

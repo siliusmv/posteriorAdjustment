890b3fe6e26d8bb86552ca0c841bab2db32d9d22
Silius M. Vandeskog <siliusv@gmail.com>
2022-09-13 08:39:36 +0200

    Update block likelihood simulation

--- exec/simulation-studies/3-block-likelihood.R
+++ exec/simulation-studies/3-block-likelihood.R
@@ -84,9 +84,6 @@ for (j in 1:(domain_size / block_size)) {
       y_lims[1] < loc[, 2] & loc[, 2] <= y_lims[2])
   }
 }
-# The block index list for the full likelihood is the list with only one index vector,
-# which contains all n_loc indices
-full_index = list(1:n_loc)
 
 # Run all the n_repl experiments
 set.seed(1)
@@ -101,101 +98,60 @@ for (i in 1:n_repl) {
   y = as.matrix(A %*% z)
   y = y + rnorm(length(y), sd = tau^-.5)
 
-  # Create the necessary objects for running R-INLA with both the
-  # full and the composite likelihoods
-  effects_blocks = list(
+  # Create the necessary objects for running R-INLA
+  effects = list(
     spatial = inla.spde.make.index("spatial", n.spde = mesh$n, n.repl = n_train * n_blocks))
-  effects_full = list(
-    spatial = inla.spde.make.index("spatial", n.spde = mesh$n, n.repl = n_train))
-  y_inla_blocks = as.numeric(y[unlist(block_index), ])
-  y_inla_full = as.numeric(y[unlist(full_index), ])
-  A_blocks = list(spatial = inla.spde.make.A(
+  y_inla = as.numeric(y[unlist(block_index), ])
+  A_inla = list(spatial = inla.spde.make.A(
     mesh = mesh,
     loc = loc,
     index = rep(unlist(block_index), n_train),
     repl = rep(1:(n_blocks * n_train), rep(sapply(block_index, length), n_train))))
-  A_full = list(spatial = inla.spde.make.A(
-    mesh = mesh,
-    loc = loc,
-    index = rep(unlist(full_index), n_train),
-    repl = rep(1:n_train, rep(sapply(full_index, length), n_train))))
-  stack_blocks = inla.stack(data = list(y = y_inla_blocks), A = A_blocks, effects = effects_blocks)
-  stack_full = inla.stack(data = list(y = y_inla_full), A = A_full, effects = effects_full)
-  formula_blocks = y ~ -1 + f(spatial, model = spde, replicate = spatial.repl, nrep = n_train * n_blocks)
-  formula_full = y ~ -1 + f(spatial, model = spde, replicate = spatial.repl, nrep = n_train)
+  stack = inla.stack(data = list(y = y_inla), A = A_inla, effects = effects)
+  formula = y ~ -1 + f(spatial, model = spde, replicate = spatial.repl, nrep = n_train * n_blocks)
 
-  # Fit the two models
-  block_fit = inla(
-    formula = formula_blocks,
-    data = inla.stack.data(stack_blocks),
+  # Fit the model
+  fit = inla(
+    formula = formula,
+    data = inla.stack.data(stack),
     family = "gaussian",
     num.threads = n_cores,
     only.hyperparam = TRUE,
     control.inla = list(control.vb = list(enable = FALSE)),
     inla.mode = "experimental",
-    control.predictor = list(A = inla.stack.A(stack_blocks)))
-  full_fit = inla(
-    formula = formula_full,
-    data = inla.stack.data(stack_full),
-    family = "gaussian",
-    num.threads = n_cores,
-    only.hyperparam = TRUE,
-    control.inla = list(control.vb = list(enable = FALSE)),
-    inla.mode = "experimental",
-    control.predictor = list(A = inla.stack.A(stack_full)))
+    control.predictor = list(A = inla.stack.A(stack)))
 
-  # Estimate θ* for both models, using only the training data
-  theta_block = block_fit$mode$theta
-  theta_full = full_fit$mode$theta
+  # Estimate θ*
+  theta = fit$mode$theta
 
   # Estimate H, J and C
-  H_block = solve(block_fit$misc$cov.intern)
-  H_full = solve(full_fit$misc$cov.intern)
-  grads_block = numDeriv::jacobian(
+  H = solve(fit$misc$cov.intern)
+  grads = numDeriv::jacobian(
     function(x) block_loglik(x, y, block_index, spde, A, return_mat = TRUE),
-    x = theta_block)
-  grads_full = numDeriv::jacobian(
-    function(x) block_loglik(x, y, full_index, spde, A, return_mat = TRUE),
-    x = theta_full)
-  J_block = 0
-  J_full = 0
+    x = theta)
+  J = 0
   for (j in 1:n_train) {
     for (k in 1:n_blocks) {
       for (l in 1:n_blocks) {
-        J_block = J_block + grads_block[(j - 1) * n_blocks + k, ] %*%
-          grads_block[(j - 1) * n_blocks + l, , drop = FALSE]
+        J = J + grads[(j - 1) * n_blocks + k, ] %*%
+          grads[(j - 1) * n_blocks + l, , drop = FALSE]
       }
     }
-    J_full = J_full + grads_full[j, ] %*% grads_full[j, , drop = FALSE]
   }
-  C_block = get_C(H_block, J_block)
-  C_full = get_C(H_full, J_full)
+  C = get_C(H, J)
 
-  # Keep only the relevant parts of the model fits, to reduce requirements on file storage
-  block_fit = list(
-    misc = block_fit$misc,
-    internal.marginals.hyperpar = block_fit$internal.marginals.hyperpar,
-    model = list(theta = block_fit$mode$theta))
-  block_fit$misc$reordering = NULL
-  block_fit$misc$family = NULL
-  block_fit$misc$linkfunctions = NULL
-  class(block_fit) = "inla"
-
-  full_fit = list(
-    misc = full_fit$misc,
-    internal.marginals.hyperpar = full_fit$internal.marginals.hyperpar,
-    model = list(theta = full_fit$mode$theta))
-  full_fit$misc$reordering = NULL
-  full_fit$misc$family = NULL
-  full_fit$misc$linkfunctions = NULL
-  class(full_fit) = "inla"
+  # Keep only the relevant parts of the model fit, to reduce requirements on file storage
+  fit = list(
+    misc = fit$misc,
+    internal.marginals.hyperpar = fit$internal.marginals.hyperpar,
+    model = list(theta = fit$mode$theta))
+  fit$misc$reordering = NULL
+  fit$misc$family = NULL
+  fit$misc$linkfunctions = NULL
+  class(fit) = "inla"
 
   # Save the temporary results
-  model_fits[[i]] = list(
-    block_fit = block_fit,
-    full_fit = full_fit,
-    C_block = C_block,
-    C_full = C_full)
+  model_fits[[i]] = list(fit = fit, C = C)
   saveRDS(model_fits, filename)
   pb$tick()
 }
@@ -213,34 +169,23 @@ pb = progress_bar(length(res))
 intervals = list()
 for (i in seq_along(res)) {
   intervals[[i]] = list()
-  for (j in 1:2) {
-    if (j == 1) {
-      fit = res[[i]]$block_fit
-      C = res[[i]]$C_block
-      model = "Block"
+  theta_uncorrected = inla.hyperpar.sample(n_posterior_samples, res[[i]]$fit, intern = TRUE)
+  for (k in 1:2) {
+    if (k == 1) {
+      theta_corrected = theta_uncorrected
+      label = "Unadjusted"
     } else {
-      fit = res[[i]]$full_fit
-      C = res[[i]]$C_full
-      model = "Full"
+      theta_corrected = matrix(rep(res[[i]]$fit$mode$theta, each = n_posterior_samples), ncol = n_theta)
+      theta_corrected = theta_corrected + (theta_uncorrected - theta_corrected) %*% t(res[[i]]$C)
+      label = "Adjusted"
     }
-    theta_uncorrected = inla.hyperpar.sample(n_posterior_samples, fit, intern = TRUE)
-    for (k in 1:2) {
-      if (k == 1) {
-        theta_corrected = theta_uncorrected
-        label = "Unadjusted"
-      } else {
-        theta_corrected = matrix(rep(fit$mode$theta, each = n_posterior_samples), ncol = n_theta)
-        theta_corrected = theta_corrected + (theta_uncorrected - theta_corrected) %*% t(C)
-        label = "Adjusted"
-      }
-      intervals[[i]][[length(intervals[[i]]) + 1]] = rbind(
-        as.data.frame(apply(theta_corrected, 2, quantile, probs = (1 - probs) / 2)) |>
-          dplyr::mutate(model = model, label = label, prob = probs, lim = "lower"),
-        as.data.frame(apply(theta_corrected, 2, quantile, probs = 1 - (1 - probs) / 2)) |>
-          dplyr::mutate(model = model, label = label, prob = probs, lim = "upper"))
-      row.names(intervals[[i]][[length(intervals[[i]])]]) = NULL
-      names(intervals[[i]][[length(intervals[[i]])]])[1:n_theta] = theta_names
-    }
+    intervals[[i]][[length(intervals[[i]]) + 1]] = rbind(
+      as.data.frame(apply(theta_corrected, 2, quantile, probs = (1 - probs) / 2)) |>
+        dplyr::mutate(label = label, prob = probs, lim = "lower"),
+      as.data.frame(apply(theta_corrected, 2, quantile, probs = 1 - (1 - probs) / 2)) |>
+        dplyr::mutate(label = label, prob = probs, lim = "upper"))
+    row.names(intervals[[i]][[length(intervals[[i]])]]) = NULL
+    names(intervals[[i]][[length(intervals[[i]])]])[1:n_theta] = theta_names
   }
   intervals[[i]] = do.call(rbind, intervals[[i]]) |>
     dplyr::mutate(iter = i)
@@ -259,53 +204,42 @@ is_inside_interval = local({
   }
   tmp = (tmp1[, 1:n_theta] & tmp2[, 1:n_theta]) |>
     as.data.frame() |>
-    dplyr::mutate(model = tmp1$model, label = tmp1$label,
-                  prob = tmp1$prob, iter = tmp1$iter)
+    dplyr::mutate(label = tmp1$label, prob = tmp1$prob, iter = tmp1$iter)
   tmp
 })
 
 # Compute coverage percentages
 tmp = is_inside_interval |>
   tidyr::pivot_longer(all_of(theta_names)) |>
-  dplyr::group_by(prob, label, model, name) |>
+  dplyr::group_by(prob, label, name) |>
   dplyr::summarise(coverage = mean(value)) |>
-  dplyr::mutate(name = factor(
-    x = name,
-    levels = theta_names,
-    labels = paste0("$\\", c("tau", "rho", "sigma"), "$"))) |>
-  tidyr::pivot_wider(names_from = name, values_from = coverage)
-tmp = tmp[1:nrow(tmp) + rep(c(2, 2, -2, -2), 3), ]
+  dplyr::mutate(
+    name = factor(
+      x = name,
+      levels = theta_names,
+      labels = paste0("$\\", c("tau", "rho", "sigma"))),
+    label = factor(
+      x = label,
+      levels = c("Unadjusted", "Adjusted"),
+      labels = c("$", "_\\text{adj}$"))) |>
+  tidyr::pivot_wider(names_from = c(name, label), values_from = coverage)
+names(tmp) = sub("_", "", names(tmp))
+tmp = tmp[, c(1, 5, 2, 6, 3, 7, 4)]
 print(tmp)
 
 # ==============================================
 # Reformat the results into latex tabular format
 # ==============================================
-table = paste(paste(c("\\(1 - \\alpha\\)", "Method", names(tmp)[-(1:2)]), collapse = " & "), "\\\\")
-table = paste(paste(
-  c("", "", "\\multicolumn{3}{c}{Block likelihood}", "\\multicolumn{3}{c}{Full likelihood}"),
-  collapse = " & "), "\\\\")
-table[2] = "\\cmidrule(r){3-5} \\cmidrule(l){6-8}"
-table[3] = paste(paste(c("\\(1 - \\alpha\\)", "Method", rep(names(tmp)[-(1:3)], 2)), collapse = " & "), "\\\\")
-table[4] = "\\midrule"
-j = 5
-for (i in seq(1, nrow(tmp) - 1, by = 2)) {
+table = paste(paste(c("Aim", names(tmp)[-1]), collapse = " & "), "\\\\")
+table[2] = "\\hline"
+j = 3
+for (i in 1:nrow(tmp)) {
   table[j] = paste(
-    c(tmp$label[i],
-      paste0("$", round(100 * tmp[i, -(1:3)], digits = 0), "\\%$"),
-      paste0("$", round(100 * tmp[i + 1, -(1:3)], digits = 0), "\\%$")),
+    c(paste0("$", round(100 * tmp$prob[i], digits = 0), "\\%$"),
+      paste0("$", round(100 * tmp[i, -1], digits = 0), "\\%$")),
     collapse = " & ")
-  if (i %% 4 == 1) {
-    table[j] = paste(
-      c(paste0("$", round(100 * tmp$prob[i], digits = 0), "\\%$"), table[j]), collapse = " & ")
-  } else {
-    table[j] = paste(c("", table[j]), collapse = " & ")
-  }
   table[j] = paste(table[j], "\\\\")
   j = j + 1
-  if (i %% 4 == 3 && i != (nrow(tmp) - 1)) {
-    table[j] = "\\midrule"
-    j = j + 1
-  }
 }
 table = paste(paste(table, collapse = "\n"), "\n")
 
